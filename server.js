65c90ea4cb5461affc6c0315dc5f2b8ead6acdb4
virajsingh5897 <virajsingh5897@gmail.com>
2024-07-26 17:18:18 +0530

    upload api

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const app = express();
 const port = 3000;
 const { Pool } = require('pg');
+const multer = require('multer');
 require('dotenv').config()
 // Enable CORS for all origins
 app.use(cors());
@@ -22,12 +23,95 @@ app.get('/', async  (req, res) => {
     res.send(result.rows[0]);
 });
 
+
+const storage = multer.memoryStorage(); // Store files in memory as Buffer objects
+const upload = multer({ storage: storage });
+
+// Image upload endpoint
+app.post('/upload', upload.single('image'), async (req, res) => {
+  try {
+    const file = req.file;
+    const fileSize = file.size;
+    const fileData = file.buffer; // The binary data of the file
+    const client = await pool.connect();
+    const result = await client.query(
+      'INSERT INTO images (filename, file_size, image_data) VALUES ($1, $2, $3) RETURNING *',
+      [file.originalname, fileSize, fileData]
+    );
+    client.release();
+    delete 
+    res.json(result.rows[0]);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Error uploading file');
+  }
+});
+
+
+// Image retrieval endpoint
+app.get('/images', async (req, res) => {
+  try {
+    const client = await pool.connect();
+    const result = await client.query('SELECT id,filename, file_size FROM images');
+    client.release();
+    if (result.rows.length > 0) {
+      const image = result.rows[0];
+      res.send(image);
+    } else {
+      res.status(404).send('Image not found');
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Error retrieving file');
+  }
+});
+
+
+
+// Image retrieval endpoint
+app.get('/image/:id', async (req, res) => {
+  try {
+    const client = await pool.connect();
+    const result = await client.query('SELECT * FROM images WHERE id = $1', [req.params.id]);
+    client.release();
+    if (result.rows.length > 0) {
+      const image = result.rows[0];
+      res.setHeader('Content-Type', 'image/jpeg'); // Adjust the MIME type as necessary
+      res.send(image.image_data);
+    } else {
+      res.status(404).send('Image not found');
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Error retrieving file');
+  }
+});
+
+
+// Image deletion endpoint
+app.delete('/image/:id', async (req, res) => {
+  try {
+    const client = await pool.connect();
+    const result = await client.query('DELETE FROM images WHERE id = $1 RETURNING *', [req.params.id]);
+    client.release();
+    if (result.rowCount > 0) {
+      res.status(200).send('Image deleted successfully');
+    } else {
+      res.status(404).send('Image not found');
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Error deleting file');
+  }
+});
+
+
 // Start the server
 app.listen(port, async () => {
   console.log(`Server is running on http://localhost:${port}`);
   const client = await pool.connect();
   const result = await client.query('SELECT NOW()');
-  console.log(result);
+  console.log('connection successful');
   client.release();
   // res.send(result.rows[0]);
 });
